45f1d97dbb05a3b05a14c04cef6c40990351360a
Duketamin <duketamin@Duketamins-MacBook-Pro.local>
2021-10-28 17:33:44 -0400

    add technologies

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiReact, DiAws } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider/>
+    <br/>
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      afaefaewf
+    </SectionText>
+    <List>
+    <ListItem>
+        <DiReact size="3rem"/>
+        <ListContainer>
+          <ListTitle>Front-end</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+      <DiFirebase size="3rem"/>
+        <ListContainer>
+          <ListTitle>Back-end</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            Node and Database
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+      <DiAws size="3rem"/>
+        <ListContainer>
+          <ListTitle>Cloud </ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            Amazon AWS
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
